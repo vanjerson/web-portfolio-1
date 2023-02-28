28efca3989b62d71e0225e7cf8f23be2fadfbb33
vanjerson <vanjerson2@gmail.com>
2023-02-28 16:54:25 +0800

    first commit

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Van_jerson_CV_2022.pdf";
+import pdf from "../../Assets/../Assets/Van-Jerson-Soria_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/vanjerson/web-portfolio-1/main/src/Assets/Van_jerson_CV_2022.pdf";
+  "https://raw.githubusercontent.com/vanjerson/web-portfolio-1/main/src/Assets/Van-Jerson-Soria_CV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
